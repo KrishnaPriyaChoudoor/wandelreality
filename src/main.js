76228c8c7fb46bed4ad59cd3836ec843e39f6183
KrishnaPriyaChoudoor <33959100+KrishnaPriyaChoudoor@users.noreply.github.com>
2025-06-25 12:01:19 +0530

    updated the events section

--- src/main.js
+++ src/main.js
@@ -15,3 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+window.togglePastEvents = function (event) {
+  const pastSection = document.getElementById("past-events");
+  const isHidden = pastSection.classList.contains("hidden");
+  pastSection.classList.toggle("hidden");
+
+  event.currentTarget.textContent = isHidden
+    ? "Hide Past Events"
+    : "View Past Events";
+};
